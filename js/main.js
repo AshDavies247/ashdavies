af789964d6a174c6ff4d9bb160ede113e95658ce
Ash Davies <ash.davies247@gmail.com>
2019-01-11 17:12:47 +0000

    Added a little more to the todo and made the weather stuff better

--- js/main.js
+++ js/main.js
@@ -1,43 +1,60 @@
 // Weather API
 
-// jason request
-var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e447b53b0468e8185e7be1f1a16daccd";
-var request = new XMLHttpRequest();
+var weatherBgArea = $('.content');
+var weatherLoc = $('[data-js-weather="loc"]');
+var weatherTemp = $('[data-js-weather="temp"]');
+var weatherCond = $('[data-js-weather="cond"]');
+var weatherControls = $('[data-js-weather="control"]');
+var weatherIndex = {
+    2 : 'thunderstorm',
+    3 : 'drizzle',
+    5 : 'rain',
+    6 : 'snow',
+    7 : 'atmosphere',
+    8 : 'clear',
+    9 : 'clouds'
+}
 
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = weatherImgControl;
+weatherBgButtons();
 
-// background image chooser
+fetch('https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=e447b53b0468e8185e7be1f1a16daccd')
+    .then(function(response) {
+        return response.json();
+  })
+    .then(function(myJson) {
+        weatherGetId(myJson);
+        weatherGetInfo(myJson);
+});
 
-var body = $('.content');
-var weatherControls = $('[data-js-weather]');
-
-function weatherImgControl() {
-
-    // arrays of the first number of each weather category  ID. e.g 2 is the first number of the weather category ID of Thunderstorms, which is actually 200 in full.
-    var weatherIndex = ['2', '3', '5', '6', '7', '8', '9'];
-
-    // corresponding weather categories to the category ID.
-    var weatherImgIndex = ['thunderstorm', 'drizzle', 'rain', 'snow', 'atmosphere', 'clear', 'clouds']
-
-    // gets the ID of the current weather condition
-    var weatherId = request.response['weather'][0]['id'];
-
-    // picks out the first number of the above weather condition ID
+function weatherGetId(data) {
+    var weatherId = data['weather'][0]['id'];
     var weatherIdShort = weatherId.toString().charAt(0);
+    weatherBgControl(weatherIdShort);
+}
 
-    // finds out which index the above number is in the weatherIndex array
-    var weatherFinder = weatherIndex.indexOf(weatherIdShort);
+function weatherBgControl(id) {
+    weatherChangeBg(id);
+};
 
-    // changes the background image of the page to the relevant weather image.
-    body.css('background-image', 'url(../media/' + weatherImgIndex[weatherFinder] + '.gif)');
-
-    // Similar logic to above but using the number of the data attribute on the weather controls grid.
-    weatherControls.on('click', function () {
-        body.css('background-image', 'url(../media/' + weatherImgIndex[$(this).data()['jsWeather']] + '.gif)');
+function weatherBgButtons() {
+    weatherControls.on('click', function (e) {
+        var control = $(e.currentTarget).attr('id');
+        weatherChangeBg(control);
     })
+};
+
+function weatherChangeBg(id) {
+    weatherBgArea.css('background-image', 'url(../media/' + weatherIndex[id] + '.gif)');
+
+}
+
+function weatherGetInfo(data) {
+    var loc = data['name'];
+    var deg = data['main']['temp'];
+    var cond = data['weather'][0]['description'];
+    weatherLoc.html(loc);
+    weatherTemp.html(deg);
+    weatherCond.html(cond);
 }
 
 // Todo list
@@ -46,15 +63,39 @@ var listInput = $('[data-js-list="input"]');
 var listSubmit = $('[data-js-list="submit"]');
 var listTodo = $('[data-js-list="todo"]');
 var listDone = $('[data-js-list="done"]');
+var itemTodo = $('[data-js-list="itemtodo"]')
+var itemDone = $('[data-js-list="itemdone"]')
 
 listSubmit.on('click', function () {
 
     var listData = listInput.val();
 
     if (listData) {
-
-        var listItem = listTodo.append($("<li>" + listData + "</li>"));
-
+        var newListItem = $("<li>" + listData + "</li>");
+        listTodo.append(newListItem);
         listInput.val('');
+        styleListItem(newListItem);
     }
-})
+});
+
+function styleListItem(li) {
+    li.attr('data-js-list','itemtodo');
+    li.addClass('list__item');
+}
+
+listTodo.on('click','li',function(e){
+    listDone.append($(e.currentTarget));
+    $(e.currentTarget).attr('data-js-list','itemdone');
+});
+
+listDone.on('click','li',function(e){
+    listTodo.append($(e.currentTarget));
+    $(e.currentTarget).attr('data-js-list','itemtodo');
+});
+
+$( window ).on('unload',function() {
+    console.log('test');
+    localStorage.setItem('test','test');
+    localStorage.setItem('test','testing');
+    localStorage.setItem('test','chest');
+  });
