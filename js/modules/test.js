64af33fb45c6b4e7482b43da96be4a4eb1ddd5fe
Ash Davies <ash.davies247@gmail.com>
2019-01-16 17:51:35 +0000

    todo restart

---
+++ js/modules/test.js
@@ -0,0 +1,20 @@
+jQuery(function($){
+
+    var utils = {
+        store: function() {
+            
+        }
+    }
+
+
+    var app = {
+        init: function() {
+            console.log('test');
+        },
+
+        test: function() {
+            console.log('Ash rules')
+        }
+    };
+    app.init();
+});
